ee1a20cfd9081f4646e79f13ea345338ee32ef62
han1202012 <octopus_truth@163.com>
2021-04-26 20:26:50 +0800

    update

--- app/src/main/java/kim/hsl/protobuf/MainActivity.kt
+++ app/src/main/java/kim/hsl/protobuf/MainActivity.kt
@@ -2,10 +2,17 @@ package kim.hsl.protobuf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tutorial.protos.AddressBook
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //AddressBook
+
     }
+
+
+
 }
