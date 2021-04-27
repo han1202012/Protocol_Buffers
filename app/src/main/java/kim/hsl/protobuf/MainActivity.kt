dbac66b163c8802281edd28531dd67414c9cabcf
han1202012 <octopus_truth@163.com>
2021-04-27 20:54:42 +0800

    update

--- app/src/main/java/kim/hsl/protobuf/MainActivity.kt
+++ app/src/main/java/kim/hsl/protobuf/MainActivity.kt
@@ -2,10 +2,15 @@ package kim.hsl.protobuf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.tutorial.protos.AddressBook
 import com.example.tutorial.protos.Person
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -46,6 +51,21 @@ class MainActivity : AppCompatActivity() {
         // 即可得到最终对象
         var addressBook: AddressBook = addressBookBuilder.build()
 
+        // 序列化操作
+        var serializeStart = System.currentTimeMillis()
+
+        // 将 addressBook 对象转为字节数组
+        var bytes: ByteArray = addressBook.toByteArray()
+
+        Log.i(TAG, "序列化耗时 ${System.currentTimeMillis() - serializeStart} ms , 序列化大小 ${bytes.size} 字节")
+
+
+        // 反序列化操作
+        var deserializeStart = System.currentTimeMillis()
+
+        var deserializeAddressBook: AddressBook = AddressBook.parseFrom(bytes)
+
+        Log.i(TAG, "反序列化耗时 ${System.currentTimeMillis() - serializeStart} ms")
     }
 
 }
